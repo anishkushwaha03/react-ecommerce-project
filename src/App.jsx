4e9a00cbfaa7a9c900636715612f156280b036db
Anish Kushwaha <anishsinghkushwaha03@gmail.com>
2026-01-18 17:17:24 +0530

    created pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 //import { useState } from 'react'
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage.jsx';
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>Test Checkout Page</div>}></Route>
+    </Routes>
   );
 }
 
