bde6d5ae49b42b4bd52e2a35014946db8614e3eb
Anish Kushwaha <anishsinghkushwaha03@gmail.com>
2026-01-19 00:20:37 +0530

    moved tracking page to react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage.jsx';
 import { CheckoutPage } from './pages/CheckoutPage.jsx';
 import { OrdersPage } from './pages/OrdersPage.jsx';
+import { TrackingPage } from './pages/TrackingPage.jsx';
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />}></Route>
       <Route path="checkout" element={<CheckoutPage />}></Route>
       <Route path="orders" element={<OrdersPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
     </Routes>
   );
 }
