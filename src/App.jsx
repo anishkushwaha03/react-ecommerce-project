bd9260d250d6cb312fcfa51219f1e533cc3dbdd4
Anish Kushwaha <anishsinghkushwaha03@gmail.com>
2026-02-16 12:41:20 +0530

    Create add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/home/HomePage.jsx';
 import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx';
-import { OrdersPage } from './pages/OrdersPage.jsx';
-import { TrackingPage } from './pages/TrackingPage.jsx';
+import { OrdersPage } from './pages/orders/OrdersPage.jsx';
+import { TrackingPage } from './pages/tracking/TrackingPage.jsx';
 import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        setCart(response.data);
-      });
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }
 
+  useEffect(() => {
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage cart={cart} />}></Route>
+      <Route path="/" element={<HomePage cart={cart} loadCart={loadCart} />}></Route>
       <Route path="checkout" element={<CheckoutPage cart={cart} />}></Route>
       <Route path="orders" element={<OrdersPage cart={cart} />}></Route>
       <Route path="tracking" element={<TrackingPage />}></Route>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,14 +4,16 @@ import { Header } from '../../components/Header.jsx';
 import { ProductsGrid } from './ProductsGrid.jsx';
 import './HomePage.css';
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/products')
-      .then((response) => {
-        setProducts(response.data);
-      });
+    const getHomeData = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    };
+
+    getHomeData();
   }, []);
 
   return (
@@ -19,7 +21,7 @@ export function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
